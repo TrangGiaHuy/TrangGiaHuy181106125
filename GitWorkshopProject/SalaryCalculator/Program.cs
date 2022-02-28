4ca8852bd5b48000e4f3baf6d78a040ab987e98e
TrangGiaHuy <72053204+TrangGiaHuy@users.noreply.github.com>
2022-02-28 09:03:50 +0700

    Update Program.cs

--- GitWorkshopProject/SalaryCalculator/Program.cs
+++ GitWorkshopProject/SalaryCalculator/Program.cs
@@ -8,7 +8,10 @@ namespace SalaryCalculator
         {
             var engineerSalary = EmployeeService.CalculateSalary(EmployeeType.Engineer, 2);
             Console.WriteLine(string.Format("Engineer's Salary: {0}", engineerSalary));
-
+            
+            var expertSalary = EmployeeService.CalculateSalary(EmployeeType.expert, 9);
+            Console.WriteLine(string.Format("expert Salary: {0}", expertSalary));
+            
             Console.ReadLine();
         }
     }
